4b2311acdc34c6e66f8ee592b629dd2fc4f73efc
Hiroharu ENDO <hiroharu@endo.vip.co.jp>
2014-09-07 18:34:46 +0900

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,8 @@ describe User do
 
   subject { @user }
 
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
@@ -19,6 +21,18 @@ describe User do
   it { should respond_to(:remember_token) }
   
   it { should be_valid }
+  it { should_not be_admin }
+
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
 
   describe "when name is not present" do
     before { @user.name = " " }
